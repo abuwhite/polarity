be16753a4dab42cabec093b5ddd26abb11a7b2b7
notabu <samyyklass@gmail.com>
2021-06-27 17:16:21 +0600

    refactoring

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -20,62 +20,62 @@ with open('tests/fixtures/result_json.txt', 'r') as file:
     json_correct = file.read()
 
 
-def test_json_make_diffs():
-    print('in test_yaml_make_diffs')
+def test_make_diffs_first():
+    print('in test_make_diffs_second')
     file1 = get_dict(FIRST_JSON)
     file2 = get_dict(SECOND_JSON)
     assert isinstance(make_diffs_representation(file1, file2), list)
 
 
-def test_yaml_make_diffs():
-    print('in test_yaml_make_diffs')
+def test_make_diffs_second():
+    print('in test_make_diffs_second')
     file1 = get_dict(FIRST_YAML)
     file2 = get_dict(SECOND_YAML)
     assert isinstance(make_diffs_representation(file1, file2), list)
 
 
-def test_json_stylish():
-    print('in test_yaml_stylish')
+def test_stylish_first():
+    print('in test_stylish_second')
     file1 = get_dict(FIRST_JSON)
     file2 = get_dict(SECOND_JSON)
     data = make_diffs_representation(file1, file2)
     assert stylish.make_stylish(data) == stylish_correct
 
 
-def test_yaml_stylish():
-    print('in test_yaml_stylish')
+def test_stylish_second():
+    print('in test_stylish_second')
     file1 = get_dict(FIRST_YAML)
     file2 = get_dict(SECOND_YAML)
     data = make_diffs_representation(file1, file2)
     assert stylish.make_stylish(data) == stylish_correct
 
 
-def test_json_plain():
-    print('in test_yaml_json')
+def test_plain_first():
+    print('in test_plain_first')
     file1 = get_dict(FIRST_JSON)
     file2 = get_dict(SECOND_JSON)
     data = make_diffs_representation(file1, file2)
     assert plain.make_plain(data) == plain_correct
 
 
-def test_yaml_plain():
-    print('in test_yaml_json')
+def test_plain_second():
+    print('in test_plain_second')
     file1 = get_dict(FIRST_YAML)
     file2 = get_dict(SECOND_YAML)
     data = make_diffs_representation(file1, file2)
     assert plain.make_plain(data) == plain_correct
 
 
-def test_json_json():
-    print('in test_yaml_json')
+def test_json_first():
+    print('in test_json_first')
     file1 = get_dict(FIRST_JSON)
     file2 = get_dict(SECOND_JSON)
     data = make_diffs_representation(file1, file2)
     assert json.make_json(data) == json_correct
 
 
-def test_yaml_json():
-    print('in test_yaml_json')
+def test_json_second():
+    print('in test_json_second')
     file1 = get_dict(FIRST_YAML)
     file2 = get_dict(SECOND_YAML)
     data = make_diffs_representation(file1, file2)
